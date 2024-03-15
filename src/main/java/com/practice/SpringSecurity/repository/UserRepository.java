8e3c4d3aa5f5a6450a89a3b4f7eb2dcd41dc890c
김교휘 <hwee000@naver.com>
2024-03-16 04:47:03 +0900

    add: 주석

--- src/main/java/com/practice/SpringSecurity/repository/UserRepository.java
+++ src/main/java/com/practice/SpringSecurity/repository/UserRepository.java
@@ -4,6 +4,6 @@ import com.practice.SpringSecurity.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    boolean existsByUsername(String username);
+    boolean existsByUsername(String username); //username가진사람 있는지 체크 true:존재
     UserEntity findByUsername(String username);
 }
