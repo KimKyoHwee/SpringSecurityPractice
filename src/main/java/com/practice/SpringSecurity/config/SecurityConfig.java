bdc4e6f961f1e16545025156487883288e6e1c33
김교휘 <hwee000@naver.com>
2024-03-17 22:14:33 +0900

    security fixed

--- src/main/java/com/practice/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/practice/SpringSecurity/config/SecurityConfig.java
@@ -35,8 +35,8 @@ public class SecurityConfig {
 //        http
 //                .csrf((auth)->auth.disable());
         http //로그아웃은 GET요청이기 때문에 csrf 토큰 검정이 안돼서 설정추가
-                .logout((auth)->auth.logoutUrl("/logout"))
-                        .logoutSuccessUrl("/")
+                .logout((auth)->auth.logoutUrl("/logout")
+                        .logoutSuccessUrl("/"));
         http  //다중 로그인 설정
                 .sessionManagement((auth)->auth
                         .maximumSessions(1)
